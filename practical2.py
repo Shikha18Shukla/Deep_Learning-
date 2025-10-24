43dabd3207812fcd12b8415b3c7b27eb89ec5949
Shikha Shukla <154722305+Shikha18Shukla@users.noreply.github.com>
2025-10-24 10:47:02 +0530

    Add files via upload

---
+++ practical2.py
@@ -0,0 +1,13 @@
+import sys
+import matplotlib 
+#matplotlib.use('Agg')
+import matplotlib.pyplot as plt 
+import numpy as np 
+xpoints=np.array([0,6])
+ypoints=np.array([0,250])
+plt.plot(xpoints , ypoints)
+
+#plt.savefig(sys.stdout.buffer)
+#sys.stdout.flush()
+plt.show()
+
